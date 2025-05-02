e378830d1ff43029329a971f9c47c8968ea3b3cb
dha-aa <172045152+dha-aa@users.noreply.github.com>
2025-05-02 14:12:49 +0000

    init agin 1.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,42 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import inject from '@rollup/plugin-inject';
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
+import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()]
-})
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: 'globalThis'
+      },
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          buffer: true
+        }),
+        NodeModulesPolyfillPlugin()
+      ]
+    }
+  },
+  build: {
+    rollupOptions: {
+      plugins: [
+        react(),
+        tailwindcss(),
+        inject({
+          Buffer: ['buffer', 'Buffer']
+        })
+      ]
+    }
+  },
+  define: {
+    global: 'globalThis'
+  },
+  resolve: {
+    alias: {
+      buffer: 'buffer',
+      process: 'process/browser'
+    }
+  }
+});
